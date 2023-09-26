eb26a495e0c276ae177e7fe69cb0eba61f9adbcc
Capnot34 <jenniferamachree2@gmail.com>
2023-09-26 13:42:04 +0000

    done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,30 +6,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
-    
-    if (!list || !*list || !(*list)->next)
-        return;
+	listint_t *current, *temp;
 
-    current = (*list)->next;
-    
-    while (current)
-    {
-        temp = current;
-        current = current->next;
-        while (temp && temp->prev && temp->prev->n > temp->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next)
-                temp->next->prev = temp->prev;
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-            if (!temp->prev)
-                *list = temp;
-            else
-                temp->prev->next = temp;
-            print_list(*list);
-        }
-    }
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		while (temp && temp->prev && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (!temp->prev)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+			print_list(*list);
+		}
+	}
 }
