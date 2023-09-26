3e971fdc77b87a4c9b0970a2b337fdac86a79f6c
Capnot34 <jenniferamachree2@gmail.com>
2023-09-26 08:53:31 +0000

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,31 +7,32 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int tmp, swapped;
+	size_t i, j;
+	int tmp, swapped;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap arr[j] and arr[j+1] */
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                swapped = 1;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
 
-                /* Print the array after each swap */
-                print_array(array, size);
-            }
-        }
-        /* If no two elements were swapped, the array is sorted */
-        if (swapped == 0)
-            break;
-    }
+			if (array[j] > array[j + 1])
+			{
+				/* Swap arr[j] and arr[j+1] */
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swapped = 1;
+
+				/* Print the array after each swap */
+				print_array(array, size)
+			}
+		}
+		/* If no two elements were swapped, the array is sorted */
+		if (swapped == 0)
+			break;
+	}
 }
