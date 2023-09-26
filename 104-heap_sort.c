07e21be13eb4a53dea642b395e9902b764d53dc6
Capnot34 <jenniferamachree2@gmail.com>
2023-09-26 20:32:23 +0000

    done

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -11,9 +11,9 @@ void heap_sort(int *array, size_t size);
  */
 void swap(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -25,21 +25,21 @@ void swap(int *a, int *b)
  */
 void heapify(int *array, size_t size, int i, size_t print_size)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
 
-    if (left < (int)size && array[left] > array[largest])
-        largest = left;
-    if (right < (int)size && array[right] > array[largest])
-        largest = right;
+	if (left < (int)size && array[left] > array[largest])
+		largest = left;
+	if (right < (int)size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != i)
-    {
-        swap(&array[i], &array[largest]);
-        print_array(array, print_size);
-        heapify(array, size, largest, print_size);
-    }
+	if (largest != i)
+	{
+		swap(&array[i], &array[largest]);
+		print_array(array, print_size);
+		heapify(array, size, largest, print_size);
+	}
 }
 
 /**
@@ -50,18 +50,18 @@ void heapify(int *array, size_t size, int i, size_t print_size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
+	int i;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, i, size);
-    for (i = size - 1; i >= 0; i--)
-    {
-        swap(&array[0], &array[i]);
-        if (i != 0)
-            print_array(array, size);
-        heapify(array, i, 0, size);
-    }
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i >= 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		if (i != 0)
+			print_array(array, size);
+		heapify(array, i, 0, size);
+	}
 }
