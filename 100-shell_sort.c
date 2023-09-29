9e59bdcb610dac298204289ca87ce21db8ab2f4d
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-09-30 00:02:49 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,16 +1,22 @@
 #include "sort.h"
 
+/**
+ * shell_sort - sorts an array of integers using shell sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
 void shell_sort(int *array, size_t size)
 {
     size_t gap, i, j;
     int temp;
 
-    for (gap = 1; gap < size; gap = gap * 3 + 1)
-        ; // Calculate gap
+    if (!array || size < 2)
+        return;
 
-    for (gap /= 3; gap > 0; gap /= 3)
+    for (gap = size / 2; gap > 0; gap /= 2)
     {
-        for (i = gap; i < size; i++)
+        for (i = gap; i < size; i += 1)
         {
             temp = array[i];
             for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
